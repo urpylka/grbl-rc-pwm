227c5105af6f6a88ac8fac6b7d9ddd4c8bf28fa4
Artem Smirnov <urpylka@gmail.com>
2021-08-14 15:15:55 +0400

    Cleanup syntax

--- grbl-rc-pwm.ino
+++ grbl-rc-pwm.ino
@@ -3,10 +3,10 @@
 Servo spindle;
 const byte interruptPin = 2;
 const byte spindlePwmPin = 3;
-int spindleOut=0;
+int spindleOut = 0;
 
-//Initialize variables that will hold the timstamps of the rising and falling edges of the input PWM signal.
-//Since those are called from within the ISRs they should be volatile
+// Initialize variables that will hold the timstamps of the rising and falling edges of the input PWM signal.
+// Since those are called from within the ISRs they should be volatile
 volatile long lastHigh = 0;
 volatile long lastLow = 0;
 volatile long highLength = 0;
@@ -24,7 +24,7 @@ void loop() {
   delay(10);
 }
 
-//ISR functions are used to read the PWM signal more accurately. Those work reliably up to about 4KHz
+// ISR functions are used to read the PWM signal more accurately. Those work reliably up to about 4KHz
 void golow() {
   attachInterrupt(digitalPinToInterrupt(interruptPin), gohigh, RISING);
   highLength = micros() - lastHigh;
